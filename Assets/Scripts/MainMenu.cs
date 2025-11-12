e8a52ce683f5f48ac499e76f4e4d90586ea600df
Daniel Felipe Coelho de Freitas <danielfreitaslele14@gmail.com>
2025-11-12 12:16:59 -0300

    Revert "Merge branch 'Shiro' of https://github.com/DaniiFreitaS/JogosDigitais-TI2 into Shiro"
    This reverts commit 09b4b161b730b805a837e273d1ace6d53461a33a, reversing
    changes made to e9576370af93ab0ae4b1f2932f224df73366d62f.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,6 @@ public class MainMenu : MonoBehaviour
 {
     [Header("Configurações")]
     [SerializeField] private string playSceneName = "GameScene";
-    [SerializeField] private GameObject TutorialPanel;
     [SerializeField] private GameObject optionsPanel;
     [SerializeField] private GameObject creditPanel;
     
@@ -32,15 +31,6 @@ public class MainMenu : MonoBehaviour
     public void CloseCredit()
     {
         creditPanel.SetActive(false);
-    }
-    public void OpenTutorial()
-    {
-        TutorialPanel.SetActive(true);
-    }
-
-    public void CloseTutorial()
-    {
-        TutorialPanel.SetActive(false);
     }
 
     public void QuitGame()
--- BackUp6/README.md
+++
@@ -1,2 +0,0 @@
-Planilha:
-https://docs.google.com/spreadsheets/d/1qjl3JskTSK6MRRGzUWKbzLXlGhmrTUfTvzAzxU_uQZU/edit?gid=0#gid=0
