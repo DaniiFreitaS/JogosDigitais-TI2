2c6d68068d3450d244f5309c0d000cc37746ed0c
VioletAscort <alicepassosduarte@gmail.com>
2025-11-15 18:59:44 -0300

    Começo implementação dos áudios
    Coloquei as musicas do menu e do runner, comecei a mexer com os sliders, mas ainda n tá funcional, n comecei os SFX . Coloquei a nova imagem do menu, faltam os botões

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------Audio Source----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------Audio Clip----------")]
+    public AudioClip background;
+    public AudioClip menu;
+    public AudioClip jump;
+    public AudioClip coin;
+
+    private void Awake()
+    {
+        // Evita duplicar
+        if (FindObjectsOfType<AudioManager>().Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        // Escuta as mudanças de cena
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene cena, LoadSceneMode modo)
+    {
+        if (cena.name == "MainMenu")
+        {
+            musicSource.clip = menu;
+        }
+        else if (cena.name == "Runner")
+        {
+            musicSource.clip = background;
+        }
+
+        musicSource.Play();
+    }
+}
